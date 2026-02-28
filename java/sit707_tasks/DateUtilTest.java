0531f7cf469d4741324a78c8949e565e5a348bb3
michaelmorks <s224437209@deakin.edu.au>
2026-02-28 13:26:59 +1100

    Add files via upload

---
+++ java/sit707_tasks/DateUtilTest.java
@@ -0,0 +1,110 @@
+package sit707_tasks;
+
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @author Ahsan Habib
+ */
+public class DateUtilTest {
+	
+	@Test
+	public void testStudentIdentity() {
+		String studentId = "s22443702";
+		Assert.assertNotNull("Student ID is null", studentId);
+	}
+
+	@Test
+	public void testStudentName() {
+		String studentName = "Michael Morks";
+		Assert.assertNotNull("Student name is null", studentName);
+	}
+
+	@Test
+	public void testMaxJanuary31ShouldIncrementToFebruary1() {
+		// January max boundary area: max+1
+		DateUtil date = new DateUtil(31, 1, 2024);
+        System.out.println("january31ShouldIncrementToFebruary1 > " + date);
+        date.increment();
+        System.out.println(date);
+        Assert.assertEquals(2, date.getMonth());
+        Assert.assertEquals(1, date.getDay());
+	}
+	
+	@Test
+	public void testMaxJanuary31ShouldDecrementToJanuary30() {
+		// January max boundary area: max-1
+		DateUtil date = new DateUtil(31, 1, 2024);
+        System.out.println("january31ShouldDecrementToJanuary30 > " + date);
+        date.decrement();
+        System.out.println(date);
+        Assert.assertEquals(30, date.getDay());
+        Assert.assertEquals(1, date.getMonth());
+	}
+	
+	@Test
+	public void testNominalJanuary() {
+		int rand_day_1_to_31 = 1 + new Random().nextInt(31);
+        DateUtil date = new DateUtil(rand_day_1_to_31, 1, 2024);
+        System.out.println("testJanuaryNominal > " + date);
+        date.increment();
+        System.out.println(date);
+	}
+	
+	/*
+	 * Complete below test cases.
+	 */
+	
+	@Test
+	public void testMinJanuary1ShouldIncrementToJanuary2() {
+		DateUtil date = new DateUtil(1,1, 2024);
+		System.out.println("January Min increment > " + date);
+		date.increment();
+		System.out.println(date);
+		
+		Assert.assertEquals(2,  date.getDay());
+		Assert.assertEquals(1, date.getMonth());
+
+	}
+	
+	@Test
+	public void testMinJanuary1ShouldDecrementToDecember31() {
+	    DateUtil date = new DateUtil(1, 1, 2024);
+	    System.out.println("January min decrement > " + date);
+	    date.decrement();
+	    System.out.println(date);
+
+	    Assert.assertEquals(31, date.getDay());
+	    Assert.assertEquals(12, date.getMonth());
+	    Assert.assertEquals(2023, date.getYear());
+	}
+	
+	/*
+	 * Write tests for rest months of year 2024.
+	 */
+	@Test
+	public void testMinJune1ShouldDecrementToMay31() {
+	    DateUtil date = new DateUtil(1, 6, 2024);
+	    date.decrement();
+
+	    Assert.assertEquals(31, date.getDay());
+	    Assert.assertEquals(5, date.getMonth());
+	}
+	
+	@Test
+	public void testMaxJune30ShouldIncrementToJuly1() {
+	    DateUtil date = new DateUtil(30, 6, 2024);
+	    date.increment();
+
+	    Assert.assertEquals(1, date.getDay());
+	    Assert.assertEquals(7, date.getMonth());
+	}
+	
+
+	
+	
+	
+}
